d6fa2ca3ad9cd6ee1deaa98ebc64418a6b898e66
ezequiasangelo <ezequias21666@hotmail.com>
2023-11-25 17:18:09 -0300

    Primeiro Commit Sequencia de Fibonacci

---
+++ Processo_seletivo/src/fibonacci/fibonacci.java
@@ -0,0 +1,21 @@
+package fibonacci;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n1 = 0, n2 = 1;
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Resultado " + n1);
+			n2 = n1 + n2;
+			n1 = n2 - n1;
+			
+			
+		}
+	
+		
+	}
+
+}

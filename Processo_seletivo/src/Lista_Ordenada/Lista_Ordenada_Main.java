3ee778dc8d346b79f423542849d8f18ae8ac3f55
ezequiasangelo <ezequias21666@hotmail.com>
2023-11-26 01:17:57 -0300

    Commit Add Busca em Lista Ordenada

---
+++ Processo_seletivo/src/Lista_Ordenada/Lista_Ordenada_Main.java
@@ -0,0 +1,50 @@
+package Lista_Ordenada;
+
+
+import java.util.Scanner;
+
+public class Lista_Ordenada_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		
+	        int[] lista = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	        
+	        System.out.print("Digite um número que seja de 1 a 10. ");
+	        int numDigitado = scanner.nextInt();	        
+	        
+	        int posicao = buscarLista(lista, numDigitado);
+	        
+	        if(posicao != -1) {
+	        	System.out.println("Seu número " + numDigitado + " está na posição " + posicao + " da lista.");
+	        }else {
+	        	System.out.println("O número " + numDigitado + " não tem na lista.");
+	        }
+	        
+	        scanner.close();
+
+	}
+
+	private static int buscarLista(int[] lista, int alvo) {
+		
+		int inicio = 0;
+        int fim = lista.length - 1;
+
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+
+            if (lista[meio] == alvo) {
+                return meio; // Retorna a posição
+            } else if (lista[meio] < alvo) {
+                inicio = meio + 1; // Descarta metade inferior
+            } else {
+                fim = meio - 1; // Descarta metade superior
+            }
+        }
+        
+        
+        return -1; 
+    }
+}
